592586a9870925fdc9f23b2a628cb61a9edb9f41
Unknown <marcin.piwek@gmail.com>
2019-03-05 12:22:54 +0100

    Changed basic config

--- mix.exs
+++ mix.exs
@@ -40,8 +40,9 @@ defmodule AAPiwek.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
-      {:guardian, "~> 1.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:guardian, "~> 1.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 
